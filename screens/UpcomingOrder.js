e6de8e439251d14aa167592844ae0ee748fb5727
seyi egunjobi <seyistry@gmail.com>
2021-08-13 10:26:42 +0100

    change scrren background

--- screens/UpcomingOrder.js
+++ screens/UpcomingOrder.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const UpcomingOrder = () => {
     return (
-        <View
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        <LinearGradient
+            colors={["#FF5B55", "#FF1161", "#FF5B55"]}
+            start={{ x: 0, y: 1 }}
+            end={{ x: 1, y: 0 }}
+            style={{ flex: 1 }}
         >
-            <Text> textInComponent </Text>
-        </View>
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    backgroundColor: "white",
+                    borderBottomLeftRadius: 30,
+                    borderBottomRightRadius: 30,
+                }}
+            >
+                <Text> textInComponent </Text>
+            </View>
+        </LinearGradient>
     );
 };
 
